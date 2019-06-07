ed93593d3d617bbe52e5119bc6f274d164adff0f
Jared Avila <jared.c.avila@gmail.com>
2019-06-07 13:11:25 -0700

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>
+        Created my free logo at{" "}
+        <a
+          href="https://logomakr.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LogoMakr.com
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
