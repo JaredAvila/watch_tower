ec2ec57abb0d0240d54412b978f8d6c81ebf5053
Jared Avila <jared.c.avila@gmail.com>
2019-06-12 14:48:11 -0700

    working on search

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,17 +1,35 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 import Media from "../Media/Media";
 
 import styles from "./SearchResults.module.css";
 
 class SearchResults extends Component {
+  state = {
+    results: [],
+    query: ""
+  };
+
+  componentDidMount() {
+    this.setState({ query: this.props.match.params.query });
+  }
+
   render() {
-    console.log(this.props.match.params.query);
+    const returned = this.state.results.map(res => {
+      return <Media media={res} />;
+    });
     return (
       <div className={styles.SearchResults}>
-        here is where the shit will go
-        <i class="fas fa-hand-middle-finger" />
+        {this.state.results.length > 0 ? (
+          { returned }
+        ) : (
+          <div>
+            <h1>No results for {this.state.query}</h1>
+            <Link to="/">Home</Link>
+          </div>
+        )}
       </div>
     );
   }
