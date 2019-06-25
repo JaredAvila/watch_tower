3060bf1aa60e975f03592678a5700fd2e2932d81
Jared Avila <jared.c.avila@gmail.com>
2019-06-25 12:29:28 -0700

    working on styles for movie container

--- src/containers/Movies/Movies.js
+++ src/containers/Movies/Movies.js
@@ -14,14 +14,23 @@ class Movies extends Component {
   render() {
     return (
       <div className={styles.Movies}>
-        <ul>
-          {this.props.comingSoon ? (
-            <MediumList
-              medium={this.props.comingSoon}
-              title="Upcoming Movies"
-            />
-          ) : null}
-        </ul>
+        <h1 className={styles.Headline}>Browse Movies</h1>
+        <div className={styles.Container}>
+          <div className={styles.Controls}>
+            <h1 className={styles.Active}>Coming Soon</h1>
+            <h1>Now Playing</h1>
+            <h1>Popular</h1>
+            <h1>Top-Rated</h1>
+          </div>
+          <div className={styles.MovieList}>
+            {this.props.comingSoon ? (
+              <MediumList
+                medium={this.props.comingSoon}
+                title="Upcoming Movies"
+              />
+            ) : null}
+          </div>
+        </div>
       </div>
     );
   }
