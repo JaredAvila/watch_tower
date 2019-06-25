39e4bed7a521af949335b6cc5004d1ab92d3a5d9
Jared Avila <jared.c.avila@gmail.com>
2019-06-25 13:50:40 -0700

    finished movies component

--- src/containers/Movies/Movies.js
+++ src/containers/Movies/Movies.js
@@ -9,23 +9,77 @@ import * as styles from "./Movies.module.css";
 
 class Movies extends Component {
   state = {
-    comingSoonClass: null,
+    comingSoonClass: styles.Active,
     nowPlayingClass: null,
     popularClass: null,
     topRatedClass: null,
-    comingSoon: false,
-    nowPlaying: false,
-    popular: false,
-    topRated: false
+    comingSoon: "block",
+    nowPlaying: "none",
+    popular: "none",
+    topRated: "none"
   };
 
   componentDidMount() {
     this.props.onFetchComingSoon();
     this.props.onFetchNowPlaying();
-    this.setState({ comingSoonClass: styles.Active });
+    this.props.onFetchTopRated();
+    this.props.onFetchPopular();
   }
 
-  componentDidUpdate() {}
+  onFilterClickHandler = filter => {
+    switch (filter) {
+      case "comingSoon":
+        this.setState({
+          comingSoon: "block",
+          nowPlaying: "none",
+          popular: "none",
+          topRated: "none",
+          comingSoonClass: styles.Active,
+          nowPlayingClass: null,
+          popularClass: null,
+          topRatedClass: null
+        });
+        break;
+      case "nowPlaying":
+        this.setState({
+          comingSoon: "none",
+          nowPlaying: "block",
+          popular: "none",
+          topRated: "none",
+          comingSoonClass: null,
+          nowPlayingClass: styles.Active,
+          popularClass: null,
+          topRatedClass: null
+        });
+        break;
+      case "popular":
+        this.setState({
+          comingSoon: "none",
+          nowPlaying: "none",
+          popular: "block",
+          topRated: "none",
+          comingSoonClass: null,
+          nowPlayingClass: null,
+          popularClass: styles.Active,
+          topRatedClass: null
+        });
+        break;
+      case "topRated":
+        this.setState({
+          comingSoon: "none",
+          nowPlaying: "none",
+          popular: "none",
+          topRated: "block",
+          comingSoonClass: null,
+          nowPlayingClass: null,
+          popularClass: null,
+          topRatedClass: styles.Active
+        });
+        break;
+      default:
+        return;
+    }
+  };
 
   render() {
     return (
@@ -33,13 +87,36 @@ class Movies extends Component {
         <h1 className={styles.Headline}>Browse Movies</h1>
         <div className={styles.Container}>
           <div className={styles.Controls}>
-            <h1 className={this.state.comingSoonClass}>Coming Soon</h1>
-            <h1 className={this.state.nowPlayingClass}>Now Playing</h1>
-            <h1 className={this.state.popularClass}>Popular</h1>
-            <h1 className={this.state.topRatedClass}>Top-Rated</h1>
+            <h1
+              onClick={() => this.onFilterClickHandler("comingSoon")}
+              className={this.state.comingSoonClass}
+            >
+              Coming Soon
+            </h1>
+            <h1
+              onClick={() => this.onFilterClickHandler("nowPlaying")}
+              className={this.state.nowPlayingClass}
+            >
+              Now Playing
+            </h1>
+            <h1
+              onClick={() => this.onFilterClickHandler("popular")}
+              className={this.state.popularClass}
+            >
+              Popular
+            </h1>
+            <h1
+              onClick={() => this.onFilterClickHandler("topRated")}
+              className={this.state.topRatedClass}
+            >
+              Top-Rated
+            </h1>
           </div>
           <div className={styles.MovieList}>
-            <div className={styles.ComingSoon}>
+            <div
+              className={styles.ComingSoon}
+              style={{ display: this.state.comingSoon }}
+            >
               {this.props.comingSoon ? (
                 <MediumList medium={this.props.comingSoon} />
               ) : (
@@ -48,17 +125,26 @@ class Movies extends Component {
                 </div>
               )}
             </div>
-            <div className={styles.NowPlaying}>
+            <div
+              className={styles.NowPlaying}
+              style={{ display: this.state.nowPlaying }}
+            >
               {this.props.nowPlaying ? (
                 <MediumList medium={this.props.nowPlaying} />
               ) : null}
             </div>
-            <div className={styles.Popular}>
+            <div
+              className={styles.Popular}
+              style={{ display: this.state.popular }}
+            >
               {this.props.popular ? (
                 <MediumList medium={this.props.popular} />
               ) : null}
             </div>
-            <div className={styles.TopRated}>
+            <div
+              className={styles.TopRated}
+              style={{ display: this.state.topRated }}
+            >
               {this.props.topRated ? (
                 <MediumList medium={this.props.topRated} />
               ) : null}
@@ -73,7 +159,9 @@ class Movies extends Component {
 const mapStateToProps = state => {
   return {
     comingSoon: state.comingSoon.comingSoonMovies,
-    nowPlaying: state.nowPlaying.nowPlaying
+    nowPlaying: state.nowPlaying.nowPlaying,
+    popular: state.popularMovies.popularMovies,
+    topRated: state.topRatedMovies.topRatedMovies
   };
 };
 
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import nowPlayingReducer from "./store/reducers/nowPlaying";
 import popularMoviesReducer from "./store/reducers/popularMovies";
+import topRatedMoviesReducer from "./store/reducers/topRatedMovies";
 import comingSoonMoviesReducer from "./store/reducers/comingSoonMovies";
 import topRatedTvReducer from "./store/reducers/topRatedTv";
 import nowOnAirReducer from "./store/reducers/nowOnAir";
@@ -19,6 +20,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const rootReducer = combineReducers({
   nowPlaying: nowPlayingReducer,
   comingSoon: comingSoonMoviesReducer,
+  topRatedMovies: topRatedMoviesReducer,
   popularMovies: popularMoviesReducer,
   topRatedTv: topRatedTvReducer,
   nowOnAir: nowOnAirReducer
